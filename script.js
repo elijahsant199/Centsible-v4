f6ff8fb871b43c27adb2f1e4085dd48bcfe63b32
elijahsant199 <elijahsant199@gmail.com>
2025-06-18 23:19:15 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js').then(
-      reg => console.log('SW registered!', reg),
-      err => console.error('SW registration failed!', err)
-    );
-  });
-}
